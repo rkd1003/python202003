22f16870cf004049f17bb8221e805e92543e1d11
rkd1003 <sayrkd1003@naver.com>
2020-03-07 16:54:10 +0900

    mariadbtest

---
+++ mariadbtest/mariatest1.py
@@ -0,0 +1,9 @@
+import pymysql
+
+conn=pymysql.connect(host='localhost',
+                        user='root'
+                        password='qwer1234'
+                        db='test'
+                        cursorclass=pymysql.cursors.DictCursor)
+
+c=conn.cursor()
---
+++ sqlitetest/sqlitetest3.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect('sqlitetest/example.db')
+c = conn.cursor()
+
+c.execute('select * from stocks')
+print(c.fetchall()) #.fetch~ 불러오다
+
+symbol = input('심볼을 입력하세요 : ')
+sql="select * from stocks where symbol='%s'"%symbol
+sql="select * from stocks where symbol=?"
+# ?은 sqlite에서 제공하는함수라
+# 다른데도 공통으로 쓰게하려면 위에 %s %symbol로 써야함
+
+c.execute(sql,(symbol,))
+# 두번째값은 튜플로 만들어 싸줘야함
+# symbol을 소괄호로 묶어 !튜플! 로 만들어줘야함
+# 튜플은 끝에 , 넣어야함
+
+print(c.fetchall())
+conn.close()
