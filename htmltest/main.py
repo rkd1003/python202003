3df3b282c074f73f7507e339ab8278d9bdfa3461
rkd1003 <sayrkd1003@naver.com>
2020-03-08 17:30:59 +0900

    웹쪽첫날

--- htmltest/main.py
+++ htmltest/main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/info')
 def index():
     return 'hello Flask'
 
-@app.route('/info')
+@app.route('/')
 def info():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8890")
+    app.run(host="0.0.0.0", port="8891",debug=True)
