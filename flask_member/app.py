c1a6eedf4663835406afe6f82ec103ffa1218164
rkd1003 <sayrkd1003@naver.com>
2020-03-15 15:20:50 +0900

    20200315일요일 강사님꺼 받기전

---
+++ flask_member/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+import pymysql, os
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('bootstraptest.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=8890)
